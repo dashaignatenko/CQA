9fb0c42a8bdeaa5594ea992cb2eaee98aef7de2d
Николай Долгов <niko.dolgow@gmail.com>
2024-03-14 19:07:10 +0300

    Added perfect prompt + perfect summaries

--- scripts/chatgpt_requests.py
+++ scripts/chatgpt_requests.py
@@ -2,6 +2,7 @@ from openai import OpenAI
 import pandas as pd
 import json
 import click
+from tqdm import tqdm
 
 
 @click.command()
@@ -16,35 +17,70 @@ def main(key: str, input_filepath: str, output_filepath: str, model: str, num_re
   prompts_df = pd.read_json(input_filepath)
   
   completions_dict = {}
-  for index, row in prompts_df.iterrows():
+  for index, row in tqdm(prompts_df.iterrows()):
     if row["Aspect"] == "":
-      content = ' '.join([f'You are an analyst, and you need to write a 300-word comparison of "{row["object1"]}" and "{row["object2"]}".',
-                  'You need to single out the better of the two.',
-                  'Please be as short and concise as possible but prioritize quality.',
-                  'Below is a list of related arguments for or against, please use only the relevant ones.',
-                  'When you use an argument, please cite its number in square brackets right after the usage.',
-                  'Below the summary, list the argument numbers you used.',
-                  f'If an argument is not relevant to "{row["object1"]}" or "{row["object2"]}" at all, do not use it.',
-                  'The needed structure is: summary (100 words),',
-                  'bullet-point list of the most topical avenues of comparison (200 words or more),',
-                  'the best option (3 words only).',
-                  'Please avoid artificial balancing of arguments.',
-                  '\nArgument list:',
+      # content = ' '.join([f'You are an analyst, and you need to write a 300-word comparison of "{row["object1"]}" and "{row["object2"]}".',
+      #             'You need to single out the better of the two.',
+      #             'Please be as short and concise as possible but prioritize quality.',
+      #             'Below is a list of related arguments for or against, please use only the relevant ones.',
+      #             'When you use an argument, please cite its number in square brackets right after the usage.',
+      #             'Below the summary, list the argument numbers you used.',
+      #             f'If an argument is not relevant to "{row["object1"]}" or "{row["object2"]}" at all, do not use it.',
+      #             'The needed structure is: summary (100 words),',
+      #             'bullet-point list of the most topical avenues of comparison (200 words or more),',
+      #             'the best option (3 words only).',
+      #             'Please avoid artificial balancing of arguments.',
+      #             '\nArgument list:',
+      #             f'{row["arguments"]}'])
+      content = ' '.join([f'You are an analyst, and you need to write a 300-word comparison of {row["object1"]} and {row["object2"]}. ',
+                  f'Task: You need to single out the better of the two. ',
+                  'Requirements: ',
+                  '- be short and concise ',
+                  '- prioritize quality ',
+                  '- analyze the list of arguments below ',
+                  '- pick relevant ones ',
+                  '- you need at least 10 arguments, create your own if needed (add a [generated] tag) ',
+                  '- cite used argument numbers in brackets right after the usage ',
+                  '- below everything, write a list of argument numbers you used ',
+                  'Needed structure: ',
+                  '- summary (100 words) ',
+                  '- bullet-point list of aspects of comparison (200 words or more) ',
+                  '- the best option (3 words only) ',
+                  '- list of used arguments ',
+                  '\nArgument list: ',
                   f'{row["arguments"]}'])
     else:
-      content = ' '.join([f'You are an analyst, and you need to write a 300-word comparison of "{row["object1"]}" and "{row["object2"]}".',
-                  f'Please focus on {row["Aspect"]}.',
-                  'You need to single out the better of the two.',
-                  'Please be as short and concise as possible but prioritize quality.',
-                  'Below is a list of related arguments for or against, please use only the relevant ones.',
-                  'When you use an argument, please cite its number in square brackets right after the usage.',
-                  'Below the summary, list the argument numbers you used.',
-                  f'If an argument is not relevant to "{row["object1"]}" or "{row["object2"]}" at all, do not use it.',
-                  'The needed structure is: summary (100 words),',
-                  'bullet-point list of the most topical avenues of comparison (200 words or more),',
-                  'the best option (3 words only).',
-                  'Please avoid artificial balancing of arguments.',
-                  '\nArgument list:',
+      # content = ' '.join([f'You are an analyst, and you need to write a 300-word comparison of "{row["object1"]}" and "{row["object2"]}".',
+      #             f'Please focus on {row["Aspect"]}.',
+      #             'You need to single out the better of the two.',
+      #             'Please be as short and concise as possible but prioritize quality.',
+      #             'Below is a list of related arguments for or against, please use only the relevant ones.',
+      #             'When you use an argument, please cite its number in square brackets right after the usage.',
+      #             'Below the summary, list the argument numbers you used.',
+      #             f'If an argument is not relevant to "{row["object1"]}" or "{row["object2"]}" at all, do not use it.',
+      #             'The needed structure is: summary (100 words),',
+      #             'bullet-point list of the most topical avenues of comparison (200 words or more),',
+      #             'the best option (3 words only).',
+      #             'Please avoid artificial balancing of arguments.',
+      #             '\nArgument list:',
+      #             f'{row["arguments"]}'])
+      content = ' '.join([f'You are an analyst, and you need to write a 300-word comparison of {row["object1"]} and {row["object2"]}. ',
+                  'Task: You need to single out the better of the two. ',
+                  f'Focus on {row["Aspect"]}. ',
+                  'Requirements: ',
+                  '- be short and concise ',
+                  '- prioritize quality ',
+                  '- analyze the list of arguments below ',
+                  '- pick relevant ones ',
+                  '- you need at least 10 arguments, create your own if needed (add a [generated] tag) ',
+                  '- cite used argument numbers in brackets right after the usage ',
+                  '- below everything, write a list of argument numbers you used ',
+                  'Needed structure: ',
+                  '- summary (100 words) ',
+                  '- bullet-point list of aspects of comparison (200 words or more) ',
+                  '- the best option (3 words only) ',
+                  '- list of used arguments ',
+                  '\nArgument list: ',
                   f'{row["arguments"]}'])
 
     completion = client.chat.completions.create(
